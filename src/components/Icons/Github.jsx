f843444fb22eb0b043a036b16b02ad25877711eb
Chris Mbah <mbahchris46@gmail.com>
2023-10-08 00:18:39 +0100

    Link icons added

---
+++ src/components/Icons/Github.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Github() {
+  return (
+    <i class="fa-brands fa-github"></i>
+  )
+}
---
+++ src/components/Icons/Gmail.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Gmail() {
+  return (
+    <i class="fa-regular fa-envelope"></i>
+  )
+}
