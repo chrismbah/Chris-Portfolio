486af6a6b1924b9e1365a90277dd732e7d41bc1d
Chris Mbah <mbahchris46@gmail.com>
2023-10-09 17:06:27 +0100

    Improved about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,22 +1,40 @@
-import React from 'react'
+import React from "react";
+import aboutImg from "../../assets/img/img1.jpg";
+import "./About.css";
 
 export default function About() {
   return (
-    <section className= "about">
-        <div className="about-img">
-            <img src="../../assets/img/img1.jpg" alt="chris" />
+    <section className="about-info">
+      <div className="about-img">
+        <img src={aboutImg} alt="chris" />
+      </div>
+    <div className="about-text">
+        <h2>
+          About <span>Me</span>
+        </h2>
+      <div className="info">
+        <p>
+          Hello! I'm Chris Mbah, a passionate Software Engineer
+          based in Lagos, Nigeria. I specialize in Front-End Development,
+          with a particular focus on React.Js, TypeScript,CSS and a wide array of
+          cutting-edge frameworks and technologies. 
+        </p>
+        <p>
+          {/* I'm dedicated to bringing digital visions to life through the power of design, development, and
+          problem-solving. */}
+          When I'm not coding or designing, you can often find me playing chess, watching anime,
+          reading manga or simply enjoying a good cup of coffee while brainstorming new ideas for my
+          next project.
+        </p>
+        <p className="last"> 
+          Let's turn your vision into reality together. Get in
+          touch, and let's create something amazing!
+        </p>
+      </div>
+      <div className="about-contact">
+        <button className="contact-btn"><a href="">Get In Touch</a></button>
         </div>
-        <div className="about-text">
-            <h2>About Me </h2>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Ex eveniet dolor exercitationem commodi numquam vel autem, 
-                possimus tempore mollitia nobis iste facere suscipit odio, 
-                itaque atque doloremque necessitatibus ad eos! Quidem hic numquam magnam id
-                 ducimus dolores laborum, a deleniti, inventore officiis voluptate nemo minima 
-                 adipisci! Reiciendis consequuntur veniam cupiditate!
-            </p>
-        </div>
+    </div>
     </section>
-  )
+  );
 }
