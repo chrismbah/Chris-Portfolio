e7b88f7c1796083d0941e573362f6f36c102f124
Chris Mbah <mbahchris46@gmail.com>
2023-10-09 17:06:40 +0100

    Minor changes

--- src/content/Content.jsx
+++ src/content/Content.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import "./Content.css"
 import Nav from '../components/Nav/Nav'
 import Home from '../components/Home/Home'
+import About from "../components/About/About"
 
 export default function Content() {
   return (
     <main className='content'>
         <Nav/>
         <Home/>
+        <About/>
     </main>
   )
 }
