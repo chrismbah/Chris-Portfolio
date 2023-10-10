4d8514e12cf7dbd00996041830e7704f2e0e941c
Chris Mbah <mbahchris46@gmail.com>
2023-10-10 16:49:56 +0100

    nav bar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,42 +1,43 @@
 import { useState } from "react";
-import  "./Nav.css"
+import "./Nav.css";
 
 export default function Nav() {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleMenu = () => {
-      setIsOpen(!isOpen);
-    };
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <>
-        <section className="nav">
-            <div className="nav-info">
-                <div className="logo">
-                    <p>chris.</p>
-                </div>
-                <ul className={`links ${isOpen? 'open-links' : ''}`}>
-                    <li className='about'>
-                        <a href="#about">About</a>
-                    </li>
-                    <li className='skills'>
-                        <a href="#skills">Skills</a>
-                    </li>
-                    <li className='projects'>
-                        <a href="#projects">Projects</a>
-                    </li>
-                    <li className='contact'>
-                        <a href="#contact">Contact</a>
-                    </li>
-                </ul>
-                <div className={`hamburger-menu ${isOpen ? 'open' : ''}`}
-                    onClick={toggleMenu} >
-                    <div className="line"></div>
-                    <div className="line"></div>
-                    <div className="line"></div>
-                </div>
-            </div>
-
-        </section>
+      <section className="nav">
+        <div className="nav-info">
+          <div className="logo">
+            <p>chris.</p>
+          </div>
+          <ul className={`links ${isOpen ? "open-links" : ""}`}>
+            <li className="about">
+              <a href="#about">About</a>
+            </li>
+            <li className="skills">
+              <a href="#skills">Skills</a>
+            </li>
+            <li className="projects">
+              <a href="#projects">Projects</a>
+            </li>
+            <li className="contact">
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+          <div
+            className={`hamburger-menu ${isOpen ? "open" : ""}`}
+            onClick={toggleMenu}
+          >
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+          </div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
