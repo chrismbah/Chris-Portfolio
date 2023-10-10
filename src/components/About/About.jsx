c12ba00daf0f4167a02433fe9fbed0e326018fb3
Chris Mbah <mbahchris46@gmail.com>
2023-10-10 16:49:46 +0100

    Major design changes ohh

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,38 +4,42 @@ import "./About.css";
 
 export default function About() {
   return (
-    <section className="about-info">
-      <div className="about-img">
-        <img src={aboutImg} alt="chris" />
-      </div>
-      <div className="about-text">
+    <>
+      <div className="header">
         <h2>
           About <span>Me</span>
         </h2>
-        <div className="info">
-          <p>
-            Hello! I'm Chris Mbah, a passionate Software Engineer based in
-            Lagos, Nigeria. I specialize in Front-End Development, with a
-            particular focus on HTML/CSS, JavaScript, React, TypeScript and a wide array of
-            cutting-edge frameworks and technologies.
-          </p>
-          <p>
-            {/* I'm dedicated to bringing digital visions to life through the power of design, development, and
-          problem-solving. */}
-            When I'm not coding or designing, you can often find me playing
-            chess, watching anime, reading manga or simply enjoying a good cup
-            of coffee.
-          </p>
-          <p className="last">
-            Get in touch, and let's create something amazing!
-          </p>
-        </div>
-        <div className="about-contact">
-          <button className="contact-btn">
-            <a href="">Get In Touch</a>
-          </button>
-        </div>
       </div>
-    </section>
+      <section className="about-info">
+        <div className="about-img">
+          <img src={aboutImg} alt="chris" />
+        </div>
+        <div className="about-text">
+          <div className="info">
+            <p>
+              Hello! I'm Chris Mbah, a passionate Software Engineer based in
+              Lagos, Nigeria. I specialize in Front-End Development, with a
+              particular focus on HTML/CSS, JavaScript, React, TypeScript and a
+              wide array of cutting-edge frameworks and technologies.
+            </p>
+            <p>
+              {/* I'm dedicated to bringing digital visions to life through the power of design, development, and
+          problem-solving. */}
+              When I'm not coding or designing, you can often find me playing
+              chess, watching anime, reading manga or simply enjoying a good cup
+              of coffee.
+            </p>
+            <p className="last">
+              Get in touch, and let's create something amazing!
+            </p>
+          </div>
+          <div className="about-contact">
+            <button className="contact-btn">
+              <a href="">Get In Touch</a>
+            </button>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
