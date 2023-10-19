783665d39a1da4720c433f7ca5cd0e27ef52445d
Chris Mbah <mbahchris46@gmail.com>
2023-10-19 17:55:06 +0100

    Project card for each project

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
-export default function ProjectCard() {
+export default function ProjectCard({ name, tools, det }) {
   return (
-    <div className='project-card'>
-      
+    <div className="project-card">
+      <h3>{name}</h3>
+      <div className="project-tools">
+        {tools.map((tool, i) => {
+          return (
+            <div key={i} className="tool-card">
+              {tool}
+            </div>
+          );
+        })}
+      </div>
+      <div className="project-info">
+        <p>{det}</p>
+      </div>
+      <div className="project-icons">
+        <div className="live"></div>
+        <div className="github"></div>
+      </div>
     </div>
-  )
+  );
 }
