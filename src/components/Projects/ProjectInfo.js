e654e8cc44bfcae5696e7359815badb1d98521c0
Chris Mbah <mbahchris46@gmail.com>
2023-10-21 14:57:46 +0100

    Added links for my projects

--- src/components/Projects/ProjectInfo.js
+++ src/components/Projects/ProjectInfo.js
@@ -2,27 +2,38 @@ export const projectInfo = [
     {
         name:"Coin Pulse",
         tools: ['JavaScript','React-Js', 'CSS', 'RESTFUL-API'],
-        details: "A crypto currency price tracker app that enables users stay updated on real-time cryptocurrency prices, market cap, and other essential information."
+        details: "A crypto currency price tracker app that enables users stay updated on real-time cryptocurrency prices, market cap, and other essential information.",
+        liveLink: "https://coin-pulse.vercel.app",
+        githubLink: "https://github.com/chrismbah/Coin-Pulse"
     },
     {
         name:"Spend Smart",
         tools: ['JavaScript','React-Js', 'Typescript'],
-        details: "A user-friendy expense tracker designed to help users manage their finances effeciently"
+        details: "A user-friendy expense tracker designed to help users manage their finances effeciently",
+        liveLink: "https://spend-smart-nine.vercel.app",
+        githubLink: "https://github.com/chrismbah/Spend-Smart"
     },  
     {
         name:"Quiz Master",
         tools: ['JavaScript','React-Js', 'CSS', 'Framer-Motion'],
-        details: "A dynamic quiz platform that offers a diverse range of questions with cool animations"
+        details: "A dynamic quiz platform that offers a diverse range of questions with cool animations",
+        liveLink: "https://quiz-master-ten.vercel.app",
+        githubLink: "https://github.com/chrismbah/Quiz-Master"
     },  
     {
         name:"Honey Foods",
         tools: ['HTML','CSS', 'JavaScript'],
-        details: "Discover pure, nature's nectar at our honey shop. From raw honey to delightful honey-infused treats."
+        details: "Discover pure, nature's nectar at our honey shop. From raw honey to delightful honey-infused treats.",
+        liveLink: "https://honey-foods.vercel.app",
+        githubLink: "https://github.com/chrismbah/Honey-Foods"
+
     },  
     {
         name:"Planet Fitness",
         tools: ['HTML','CSS','JavaScript'],
-        details: "A Fitness App crafted with JavaScript and CSS, simplifying workout tracking and goal setting."
+        details: "A Fitness App crafted with JavaScript and CSS, simplifying workout tracking and goal setting.",
+        liveLink: "https://planet-fitness-phi.vercel.app",
+        githubLink: "https://github.com/chrismbah/Planet-Fitness"
     },
 
 ]
