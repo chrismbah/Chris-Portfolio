cea8906bce239bcecf7fdd8f8e70036a7d5a495a
Chris Mbah <mbahchris46@gmail.com>
2023-10-22 13:42:23 +0100

    Contact section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import "./Contact.css"
+import React from "react";
+import "./Contact.css";
+import Plane from "../../assets/Icons/Plane";
 
 export default function Contact() {
   return (
-    <main id='contact'>
-        <section className="contact-info">
-            contact
-        </section>
+    <main id="contact">
+      <section className="contact-info">
+        <div className="text">
+          <div className="header">
+            <h2>Contact Me</h2>
+            <p><div className="header-bar"></div>Get In Touch</p>
+          </div>
+          <p>
+            If you have any questions, project ideas, or just want to chat about
+            anything creative, <br />
+            I'm here for it. Feel free to reach out. I'm always up for a
+            friendly <br />
+            conversation or potential collaborations!
+          </p>
+        </div>
+        <button className="contact-btn">
+          Send Message <Plane />
+        </button>
+      </section>
     </main>
-  )
+  );
 }
