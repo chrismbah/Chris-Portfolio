a6f3bc4f962b8bfcf4e88c22d2c6040232796dbd
Chris Mbah <mbahchris46@gmail.com>
2023-10-28 08:27:20 +0100

    refactored CSS code

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import About from "./components/About/About";
 import Skills from "./components/Skills/Skills";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
+import Home from "./components/Home/Home";
 
 export default function App() {
   return (
